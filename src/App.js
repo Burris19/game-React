4d3bc1a9401e08ded67ce5222478978d5edd1e11
Miguel Hernandez <julianhernandez19@gmail.com>
2019-10-23 22:42:35 -0600

    [ADD] create way with divs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  _renderDivs = (alias, type) => {
+    const elements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    return elements.map((element, index) => {
+      return (
+        <div
+          className={`col-1`}
+          key={index}
+          id={`#${alias}-${element}`}
+          style={{ 'background': 'red' }}>
+          {this._renderImage(type, index)}
+        </div>
+      )
+    })
+  }
+
+  _renderImage = (type, index) => {
+    if (type === 'row') {
+      return (
+        <img
+          src="/images/way.jpg"
+          alt="Smiley face"
+          width="100"
+          height="75" />
+      )
+    } else {
+      const allowIndex = [0, 8]
+      if (allowIndex.includes(index)) {
+        return (
+          <img
+            src="/images/way.jpg"
+            alt="Smiley face"
+            width="100"
+            height="75" />
+        )
+      }
+    }
+  }
+
+  _renderMap = () => {
+    const rows = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    return rows.map((row) => {
+      const type = row === 'one' || row === 'nine' ? 'row' : 'colum'
+      return (
+        <div className="row">
+          {this._renderDivs(row, type)}
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this._renderMap()}
+      </div>
+    )
+  }
 }
+
 
 export default App;
